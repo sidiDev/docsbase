e4f07c99b5e2fd651fc0e4a5a0905112a81a4383
sidiDev <sididev3@gmail.com>
2025-11-16 09:57:23 +0000

    Update Sentry DSN for error tracking in instrument.server.mjs to a new endpoint for improved monitoring.

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/tanstackstart-react";
 
 Sentry.init({
-  dsn: "https://5ceff1e511e1aba1df2f925bb07f1e46@o4508194135146496.ingest.de.sentry.io/4510373993119824",
+  dsn: "https://70fdbe0e9b78cc6c3eacbe1d415a1e94@o4508194135146496.ingest.de.sentry.io/4510374016450640",
 
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
